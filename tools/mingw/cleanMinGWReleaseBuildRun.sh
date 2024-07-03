37b321321dd3cebd7c1524007807a767449918e9
Ilia Prokhorov <demensdeum@gmail.com>
2024-07-03 05:08:51 +0400

    MinGW build not static anymore

--- tools/mingw/cleanMinGWReleaseBuildRun.sh
+++ tools/mingw/cleanMinGWReleaseBuildRun.sh
@@ -17,5 +17,14 @@ cp samples/helloWorld/main.js release
 cp build/NixyPlayer.exe release
 cp extensions/base/build/NixyPlayerBaseExtension.dll release
 cp resources/Windows_extensions.nixyp release/extensions.nixyp
+cp /usr/i686-w64-mingw32/bin/libwinpthread-1.dll release
+cp /usr/i686-w64-mingw32/bin/libgcc_s_dw2-1.dll release
+cp /usr/i686-w64-mingw32/bin/libstdc++-6.dll release
+
 cd release
+
 wine NixyPlayer.exe main.js --verbose
+
+if [ $? -eq 0 ]; then
+    zip -r NixyPlayer-1.0.0.zip .
+fi
